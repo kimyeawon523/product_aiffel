b9bf428671aa4b0dec20a3e626e703142117abd4
man-ze <ip0123130@gmail.com>
2026-01-12 15:41:04 +0900

    Account 문제 풀이

---
+++ Training01_Order.py
@@ -0,0 +1,10 @@
+# class Order 생성
+
+# 생성자 매개변수 3개(order_number, order_amount, order_status)
+#                   주문 번호        주문 금액      주문 상태
+
+# getter/setter 작성
+#  - order_amount의 값을 수정할 때 주문 금액이 100.0 미만이면
+#    InvalidOrderError 발생 ("유효하지 않은 금액입니다")
+#  - order_amount의 값을 수정할 때 주문 금액이 100.0 이상이면 모든 멤버 변수 출력
+
